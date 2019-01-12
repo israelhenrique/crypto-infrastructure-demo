ef62150facf0f5ea970b7516abb39e5c87ffcc28
Israel Lima <israelhenrique@protonmail.com>
2019-01-12 19:12:38 -0200

    change profile and buckets names

--- management/backend.tf
+++ management/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     encrypt = "false"
-    bucket  = "crypto-resources-terraform"
+    bucket  = "crypto-resources-terraform-israel"
     key     = "management/terraform.tfstate"
     region  = "us-east-2"
-		profile = "xdevel"
+    profile = "CryptoArb"
   }
 }
